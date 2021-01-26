2641ee5ddf00b2fb883ee49f3c712d556d957bd7
Ali Khan <ali.khan@startrum.com>
2021-01-26 16:33:47 +0500

    router guard implemented (vue router section ended)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import UserProfile from '../views/UserProfile.vue'
+import Admin from '../views/Admin.vue'
 
 const routes = [
   {
@@ -12,6 +13,14 @@ const routes = [
     path: '/user/:userId',
     name: 'UserProfile',
     component: UserProfile
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta: {
+      requiredAdmin: true
+    }
   }
 ]
 
@@ -19,6 +28,15 @@ const router = createRouter({
   // we changed createWebHashHistory to createWebHistory to overcome that extra # in url
   history: createWebHistory(),
   routes
+})
+
+// router guard
+router.beforeEach(async (to, from, next) => {
+  const isAdmin = false
+  const requiredAdmin = to.matched.some(record => record.meta.requiredAdmin)
+
+  if (requiredAdmin && !isAdmin) next({ name: 'Home' })
+  else next()
 })
 
 export default router
